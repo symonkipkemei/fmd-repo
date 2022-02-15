bf2893999da592c030ac41b9bbbe3faffaee61c2
symonkipkemei <symonkipkemei@gmail.com>
2022-02-15 23:41:14 +0300

    updates

---
+++ main.py
@@ -0,0 +1,12 @@
+
+from parameters import *
+
+print("*******FORMODE REPOSITORY**************")
+print("Record New project")
+print("1) Date of project commencement\n"
+      "2) Date of project completion")
+
+user_selection = int(input("insert option"))
+
+if user_selection == 1:
+    date_commence = recording_date()
