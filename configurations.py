fc9331516bed30707b5095dc7f8aeeb7706e4fd7
symonkipkemei <symonkipkemei@gmail.com>
2022-02-20 08:52:05 +0300

    major update

--- configurations.py
+++ configurations.py
@@ -1,179 +1,84 @@
+def project_configuration(filename, filetype):
+    """The following function helps you modify the project parameters,
+    by allowing you to add or remove the individual items"""
 
-def project_category_derived():
-    """Should be derived based on the typology of the project, the user should be
-    able to edit this add or subtract"""
-    global user_index, user_category
+    global user_index, user_item
     import csv
 
-    pesa_dict = {}
-    correct = True
-    while correct:
-        select = str.lower(input("Insert project category (y/n):"))
+    # Look at what is available first
+    with open(filename, "r") as f:
+        iterable = csv.reader(f)
+        dd_iterable = list(iterable)
+        index_list = []
+        source_list = []
+        # store the index and the category
+        for item in dd_iterable:
+            index_list.append(item[0])
+            source_list.append(item[1])
 
-        # Look at what is available first
-        with open("files/project_category.csv", "r") as f:
-            iterable = csv.reader(f)
-            dd_iterable = list(iterable)
-            index_list = []
-            category_list = []
-            # store the index and the category
-            for item in dd_iterable:
-                index_list.append(item[0])
-                category_list.append(item[1])
+    print(f"\n*****modify {filetype}****\n"
+          f"1). Add \n"
+          f"2). Delete")
 
-        # if yes, lets check if our entry is already used
-        if select == str.lower("y"):
-            # check if index is already used
-            try_again_index = True
-            while try_again_index:
-                user_index = str(input("\ninsert index:"))
-                if user_index in index_list:
-                    print(f"{user_index} already used, try another.")
-                # it should be a digit
-                elif not user_index.isdigit():
-                    print(f"{user_index} is not a digit, try again.")
-                # test passed , user_index released for use
-                else:
-                    try_again_index = False
+    user_selection = int(input("option : "))
 
-            # check if category already used
-            try_again_category = True
-            # subjecting the user category to test
-            while try_again_category:
-                user_category = str.upper(input("insert new category:"))
-                if user_category in category_list:
-                    print(f"{user_category} already used, try again.")
-                else:
-                    print(f"{user_category} recorded")
-                    try_again_category = False
+    if user_selection == 1:
+        print(f"\n*****adding {filetype}****")
+        # check if index is already used
+        try_again_index = True
+        while try_again_index:
+            user_index = str(input(f"insert index:"))
+            if user_index in index_list:
+                print(f"{user_index} already used, try another.")
+            # it should be a digit
+            elif not user_index.isdigit():
+                print(f"{user_index} is not a digit, try again.")
+            # 0 is already used
+            elif user_index == str(0):
+                print("0 already used, try another.")
 
-            # store data
-            data = str(user_index) + "," + str(user_category) + "\n"
-            with open("files/project_category.csv", "a") as f:
+            # test passed , user_index released for use
+            else:
+                try_again_index = False
+
+        # check if category already used
+        try_again_category = True
+        # subjecting the user category to test
+        while try_again_category:
+            user_item = str.upper(input(f"insert item:"))
+            if user_item in source_list:
+                print(f"{user_item} already used, try again.")
+            else:
+                print(f"{user_item} recorded")
+                try_again_category = False
+
+        # store data
+        data = str(user_index) + "," + str(user_item) + "\n"
+        with open(filename, "a") as f:
+            f.write(data)
+
+    elif user_selection == 2:
+        print(f"\n*****deleting {filetype} ****")
+        delete_index = int(input("insert index: "))
+        tmp_list = []
+
+        # isolating the identified item (death by isolation)
+        for x in dd_iterable:
+            if delete_index == int(x[0]):
+                print(f"{x[1]} deleted")
+            else:
+                tmp_list.append(x)
+
+        # empty the csv file , anticipating the new kids in town
+        f = open(filename, "w")
+        f.close()
+
+        # overriding the csv with the new generation
+        for y in tmp_list:
+            data = str(y[0]) + "," + str(y[1]) + "\n"
+            with open(filename, "a") as f:
                 f.write(data)
-
-        elif select == str.lower("n"):
-            correct = False
-
-        else:
-            print("Wrong input, Try again")
+    else:
+        print("Wrong input, Try again")
 
 
-def project_source_derived():
-    """Establish the source of the project"""
-
-    global user_index, user_source
-    import csv
-
-    pesa_dict = {}
-    correct = True
-    while correct:
-        select = str.lower(input("Insert project source (y/n):"))
-
-        # Look at what is available first
-        with open("files/project_source.csv", "r") as f:
-            iterable = csv.reader(f)
-            dd_iterable = list(iterable)
-            index_list = []
-            source_list = []
-            # store the index and the category
-            for item in dd_iterable:
-                index_list.append(item[0])
-                source_list.append(item[1])
-
-        # if yes, lets check if our entry is already used
-        if select == str.lower("y"):
-            # check if index is already used
-            try_again_index = True
-            while try_again_index:
-                user_index = str(input("\ninsert index:"))
-                if user_index in index_list:
-                    print(f"{user_index} already used, try another.")
-                # it should be a digit
-                elif not user_index.isdigit():
-                    print(f"{user_index} is not a digit, try again.")
-                # test passed , user_index released for use
-                else:
-                    try_again_index = False
-
-            # check if category already used
-            try_again_category = True
-            # subjecting the user category to test
-            while try_again_category:
-                user_source = str.upper(input("insert new source:"))
-                if user_source in source_list:
-                    print(f"{user_source} already used, try again.")
-                else:
-                    print(f"{user_source} recorded")
-                    try_again_category = False
-
-            # store data
-            data = str(user_index) + "," + str(user_source) + "\n"
-            with open("files/project_source.csv", "a") as f:
-                f.write(data)
-
-        elif select == str.lower("n"):
-            correct = False
-
-        else:
-            print("Wrong input, Try again")
-
-
-def project_scope_of_work_derived():
-    """Scope of work established """
-
-    global user_index, scope
-    import csv
-
-    pesa_dict = {}
-    correct = True
-    while correct:
-        select = str.lower(input("Insert project scope of work (y/n):"))
-
-        # Look at what is available first
-        with open("files/project_scope.csv", "r") as f:
-            iterable = csv.reader(f)
-            dd_iterable = list(iterable)
-            index_list = []
-            scope_list = []
-            # store the index and the category
-            for item in dd_iterable:
-                index_list.append(item[0])
-                scope_list.append(item[1])
-
-        # if yes, lets check if our entry is already used
-        if select == str.lower("y"):
-            # check if index is already used
-            try_again_index = True
-            while try_again_index:
-                user_index = str(input("\ninsert index:"))
-                if user_index in index_list:
-                    print(f"{user_index} already used, try another.")
-                # it should be a digit
-                elif not user_index.isdigit():
-                    print(f"{user_index} is not a digit, try again.")
-                # test passed , user_index released for use
-                else:
-                    try_again_index = False
-
-            # check if category already used
-            try_again_category = True
-            # subjecting the user category to test
-            while try_again_category:
-                scope = str.upper(input("insert new scope of work:"))
-                if scope in scope_list:
-                    print(f"{scope} already used, try again.")
-                else:
-                    print(f"{scope} recorded")
-                    try_again_category = False
-
-            # store data
-            data = str(user_index) + "," + str(scope) + "\n"
-            with open("files/project_scope.csv", "a") as f:
-                f.write(data)
-
-        elif select == str.lower("n"):
-            correct = False
-
-        else:
-            print("Wrong input, Try again")
