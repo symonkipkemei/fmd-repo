860cfd2873304b5fd17057dc41804a99d0ea6ba7
symonkipkemei <symonkipkemei@gmail.com>
2022-02-25 08:54:53 +0300

    work flow

--- main.py
+++ main.py
@@ -18,108 +18,120 @@ def main():
     from database import mark_active_complete
     from database import retrieve_source_user
 
-    print("*******FORMODE REPOSITORY**************")
-    print("1) Add project to database\n"
-          "2) Check earnings\n"
-          "3) Completed projects\n"
-          "4) Active projects\n"
-          "5) Settings")
+    correct = True
+    while correct:
+        print("*******FORMODE REPOSITORY**************")
+        print("1) Add project to database\n"
+              "2) Check earnings\n"
+              "3) Completed projects\n"
+              "4) Active projects\n"
+              "5) Settings\n"
+              "6) Quit")
+        print("**************************************")
+        user_selection = int(input("insert option: "))
 
-    user_selection = int(input("insert option: "))
+        if user_selection == 1:
+            # Collecting the parameters
 
-    if user_selection == 1:
-        # Collecting the parameters
+            # project category
+            client_name = client_name()
 
-        # project category
-        client_name = client_name()
+            # project category
+            filename = "files/project_category.csv"
+            filetype = "project category"
+            project_category = project_setup(filename, filetype)
 
-        # project category
-        filename = "files/project_category.csv"
-        filetype = "project category"
-        project_category = project_setup(filename, filetype)
+            # project source
+            filename = "files/project_source.csv"
+            filetype = "project source"
+            project_source = project_setup(filename, filetype)
 
-        # project source
-        filename = "files/project_source.csv"
-        filetype = "project source"
-        project_source = project_setup(filename, filetype)
+            # project scope
+            filename = "files/project_scope.csv"
+            filetype = "project scope"
+            project_scope = project_setup(filename, filetype)
 
-        # project scope
-        filename = "files/project_scope.csv"
-        filetype = "project scope"
-        project_scope = project_setup(filename, filetype)
+            # project commencement date
+            print("\n****PROJECT DATE COMMENCEMENT*****")
+            date_commencement = project_date()
 
-        # project commencement date
-        print("\n****PROJECT DATE COMMENCEMENT*****")
-        date_commencement = project_date()
+            # project status
+            filename = "files/project_status.csv"
+            filetype = "project status"
+            project_status = project_setup(filename, filetype)
 
-        # project status
-        filename = "files/project_status.csv"
-        filetype = "project status"
-        project_status = project_setup(filename, filetype)
+            if project_status == "COMPLETE":
+                # project completion date
+                print("\n****PROJECT DATE COMPLETION*****")
+                date_completion = project_date()
+                # project fund
+                project_fund = project_funds()
+            else:
+                project_fund = 0
+                date_completion = None
 
-        if project_status == "COMPLETE":
-            # project completion date
-            print("\n****PROJECT DATE COMPLETION*****")
-            date_completion = project_date()
-            # project fund
-            project_fund = project_funds()
-        else:
-            project_fund = 0
-            date_completion = None
+            # project user
+            filename = "files/project_done_by.csv"
+            filetype = "project bee"
+            project_user = project_setup(filename, filetype)
 
-        # project user
-        filename = "files/project_done_by.csv"
-        filetype = "project bee"
-        project_user = project_setup(filename, filetype)
-
-        # project name ( project key/unique identifier)
-        project_name = project_name(date_commencement, client_name, str.lower(project_category))
-
-        # project funds
-        company_fund, brian_income, symon_income, other_income, tax = project_funds_distribution(project_source,
-                                                                                                 project_user,
-                                                                                                 project_fund)
-
-        # feeding to the database
-        create_tables()
-        insert_project_details_table(client_name, project_category, project_source, project_scope, date_commencement,
-                                     date_completion, project_name, project_status)
-        insert_project_funds_table(project_name, project_user, project_fund, company_fund, brian_income, symon_income,
-                                   other_income, tax)
-    # check earnings
-    elif user_selection == 2:
-        view_salary()
-    # view complete projects
-    elif user_selection == 3:
-        view_projects()
-
-    # view active projects
-    elif user_selection == 4:
-        option = active_projects()
-        if option is None:
-            print("Thanks")
-        else:
-            # project completion date
-            print("\n****PROJECT DATE COMPLETION*****")
-            date_completion = project_date()
-
-            # project fund
-            project_fund = project_funds()
-            project_source, project_user = retrieve_source_user(option)
+            # project name ( project key/unique identifier)
+            project_name = project_name(date_commencement, client_name, str.lower(project_category))
 
             # project funds
             company_fund, brian_income, symon_income, other_income, tax = project_funds_distribution(project_source,
                                                                                                      project_user,
                                                                                                      project_fund)
 
-            mark_active_complete(option, date_completion, project_fund, company_fund, symon_income, brian_income,
-                                 other_income, tax)
+            # feeding to the database
+            create_tables()
+            insert_project_details_table(client_name, project_category, project_source, project_scope,
+                                         date_commencement,
+                                         date_completion, project_name, project_status)
+            insert_project_funds_table(project_name, project_user, project_fund, company_fund, brian_income,
+                                       symon_income,
+                                       other_income, tax)
+        # check earnings
+        elif user_selection == 2:
+            view_salary()
+        # view complete projects
+        elif user_selection == 3:
+            view_projects()
 
+        # view active projects
+        elif user_selection == 4:
+            option = active_projects()
+            if option is None:
+                print("Thanks")
+            else:
+                # project completion date
+                print("\n****PROJECT DATE COMPLETION*****")
+                date_completion = project_date()
 
+                # project fund
+                project_fund = project_funds()
+                project_source, project_user = retrieve_source_user(option)
 
-    # settings
-    elif user_selection == 5:
-        alter_table()
+                # project funds
+                company_fund, brian_income, symon_income, other_income, tax = project_funds_distribution(project_source,
+                                                                                                         project_user,
+                                                                                                         project_fund)
+
+                mark_active_complete(option, date_completion, project_fund, company_fund, symon_income, brian_income,
+                                     other_income, tax)
+
+        # settings
+        elif user_selection == 5:
+            alter_table()
+
+        # settings
+        elif user_selection == 6:
+            correct = False
+            print("""Unless the Lord builds the house,
+    those who build it labor in vain.\nThank you!""")
+
+        else:
+            print("Wrong input, try again")
 
 
 main()
