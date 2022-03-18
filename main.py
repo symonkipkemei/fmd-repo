675815704ff48e89b493666d5a44fedf7ebe1696
symonkipkemei <symonkipkemei@gmail.com>
2022-03-18 22:18:03 +0300

    shortened

--- main.py
+++ main.py
@@ -1,22 +1,13 @@
 def main():
     """ engine of the programme"""
 
-    from parameters import client_name
-    from parameters import project_setup
-    from parameters import project_date
-    from parameters import project_name
-    from parameters import project_funds
-    from parameters import project_funds_distribution
-
-    from database import create_tables
-    from database import insert_project_funds_table
-    from database import insert_project_details_table
     from database import view_salary
     from database import view_projects
     from database import alter_table
-    from database import active_projects
-    from database import mark_active_complete
-    from database import retrieve_source_user
+
+    from parameters import time_id
+
+    from sub_programs import add_project_to_database, view_active_projects, formode_funds
 
     correct = True
     while correct:
@@ -25,107 +16,32 @@ def main():
               "2) Check earnings\n"
               "3) Completed projects\n"
               "4) Active projects\n"
-              "5) Settings\n"
-              "6) Quit")
+              "5) Formode Fund\n"
+              "6) Settings\n"
+              "7) Quit")
         print("**************************************")
         user_selection = int(input("insert option: "))
 
         if user_selection == 1:
-            # Collecting the parameters
+            add_project_to_database()
 
-            # project category
-            name_client = client_name()
-
-            # project category
-            filename = "files/project_category.csv"
-            filetype = "project category"
-            project_category = project_setup(filename, filetype)
-
-            # project source
-            filename = "files/project_source.csv"
-            filetype = "project source"
-            project_source = project_setup(filename, filetype)
-
-            # project scope
-            filename = "files/project_scope.csv"
-            filetype = "project scope"
-            project_scope = project_setup(filename, filetype)
-
-            # project commencement date
-            print("\n****PROJECT DATE COMMENCEMENT*****")
-            date_commencement = project_date()
-
-            # project status
-            filename = "files/project_status.csv"
-            filetype = "project status"
-            project_status = project_setup(filename, filetype)
-
-            if project_status == "COMPLETE":
-                # project completion date
-                print("\n****PROJECT DATE COMPLETION*****")
-                date_completion = project_date()
-                # project fund
-                project_fund = project_funds()
-            else:
-                project_fund = 0
-                date_completion = None
-
-            # project user
-            filename = "files/project_done_by.csv"
-            filetype = "project bee"
-            project_user = project_setup(filename, filetype)
-
-            # project name ( project key/unique identifier)
-            name_project = project_name(date_commencement, name_client, str.lower(project_category))
-
-            # project funds
-            company_fund, brian_income, symon_income, other_income, tax = project_funds_distribution(project_source,
-                                                                                                     project_user,
-                                                                                                     project_fund)
-
-            # feeding to the database
-            create_tables()
-            insert_project_details_table(name_client, project_category, project_source, project_scope,
-                                         date_commencement,
-                                         date_completion, name_project, project_status)
-            insert_project_funds_table(name_project, project_user, project_fund, company_fund, brian_income,
-                                       symon_income,
-                                       other_income, tax)
         # check earnings
         elif user_selection == 2:
             view_salary()
         # view complete projects
         elif user_selection == 3:
             view_projects()
-
         # view active projects
         elif user_selection == 4:
-            option = active_projects()
-            if option is None:
-                print("Thanks")
-            else:
-                # project completion date
-                print("\n****PROJECT DATE COMPLETION*****")
-                date_completion = project_date()
-
-                # project fund
-                project_fund = project_funds()
-                project_source, project_user = retrieve_source_user(option)
-
-                # project funds
-                company_fund, brian_income, symon_income, other_income, tax = project_funds_distribution(project_source,
-                                                                                                         project_user,
-                                                                                                         project_fund)
-
-                mark_active_complete(option, date_completion, project_fund, company_fund, symon_income, brian_income,
-                                     other_income, tax)
-
-        # settings
+            view_active_projects()
+        # formode fund
         elif user_selection == 5:
-            alter_table()
-
+            formode_funds()
         # settings
         elif user_selection == 6:
+            alter_table()
+        # Quit
+        elif user_selection == 7:
             correct = False
             print("""Unless the Lord builds the house,
     those who build it labor in vain.\nThank you!""")
@@ -135,16 +51,3 @@ def main():
 
 
 main()
-
-# see you on monday for greater works!
-
-# unless the lord builds the hous, those who build it labour in vain.
-
-# this week I have been slow,hope next week things pick up
-
-# we live to fight another day
-# updates
-
-
-# working on updates as from tomorrow
-# tomorrow tunaanza ya ukweli
